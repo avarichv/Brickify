9b1d1d830d4f2927e0187a225ba3a91fd8a4d2c3
clayton@gmail.com <avarichv@gmail.com>
2018-04-01 22:33:40 +0800

    Slice on tree

--- src/derivatives/treeview/index.jsx
+++ src/derivatives/treeview/index.jsx
@@ -4,15 +4,18 @@ import PropTypes from 'prop-types';
 import { Brickify, inherits } from '../../utils/brickify';
 import Flatten from '../../components/flatten';
 import IndentLabel from '../../components/indentLabel';
+import Slice from '../../components/slice';
 import List from '../../components/list';
 
-const Bricks = Brickify([Flatten, IndentLabel, List]);
+const Bricks = Brickify([Flatten, IndentLabel, List, Slice]);
 
 const TreeView = ({data, config}) => (
     <Bricks.Flatten data={data} getSubNodes={node => node.items || []}>
-        <Bricks.List getKey={i => i.data.id}>
-            <Bricks.IndentLabel matchProps={(d, c, s) => ({data: d.data.id, config: { indent: d.level }})} />
-        </Bricks.List>
+        <Bricks.Slice offset={0} bufferSize={20}>
+            <Bricks.List getKey={i => i.data.id}>
+                <Bricks.IndentLabel matchProps={(d, c, s) => ({data: d.data.id, config: { indent: d.level }})} />
+            </Bricks.List>
+        </Bricks.Slice>
     </Bricks.Flatten>
 );
 
